80a967d3e5f06b4435cfb8e5fb4d7bffe5421045
18320444515 <32980506+18320444515@users.noreply.github.com>
2019-02-16 21:21:30 +0800

    卡在了瓶颈
    模型虽然导入了，但却只是曲面图片，并不是几何体。
    
    TODO：
    1. 模型的"实体化"，使它不再是一张可穿越的曲面图片；
    2. 人物模型与游戏逻辑（打点与计时机制）；
    3. 细节小功能：时间进度的显示、定向地图的显示（如按着Tab键显示）；

---
+++ _uiPart/scripts/game/ballWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ballWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 给按钮添加事件
+		Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(enterWorld);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void enterWorld(){
+		// 跳转到滚球世界
+	    SceneManager.LoadScene("ballWorld");
+	}
+}
